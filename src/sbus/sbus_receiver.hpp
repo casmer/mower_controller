8aa47614bebe8996449b00cd2d6c12ba72a2cc6f
Casey Gregoire <caseyg@lalosoft.com>
2025-09-01 02:41:28 -0400

    creating class for sbus handling, and updating debug prints to be less noisy

---
+++ src/sbus/sbus_receiver.hpp
@@ -0,0 +1,44 @@
+// MIT License
+//  Copyright (c) 2025 Cotsbotics
+//  Author: Casey Gregoire <caseyg@lalosoft.com>
+
+#ifndef SBUS_SBUS_RECEIVER_HPP
+#define SBUS_SBUS_RECEIVER_HPP
+
+#include <Arduino.h>
+#include "sbus.h"
+
+namespace cotsbotics
+{
+    class SbusReceiver
+    {
+    public:
+        SbusReceiver()
+        {
+        }
+
+        bool no_data() { return m_no_data_received; };
+        void Begin();
+
+        void tick();
+
+        uint16_t get_channel(int8_t channel)
+        {
+            if (channel < 0 || channel >= data.NUM_CH)
+                return 0;
+            return data.ch[channel];
+        };
+
+        bool get_lost_frame() { return data.lost_frame; };
+        bool get_failsafe() { return data.failsafe; };
+
+    private:
+        bool m_no_data_received{true};
+        /* SBUS object, reading SBUS */
+        bfs::SbusRx sbus_rx{&Serial2, true};
+        /* SBUS data */
+        bfs::SbusData data;
+    };
+}; // cotsbotics
+
+#endif // SBUS_SBUS_RECEIVER_HPP
---
+++ src/sbus/sbus_reciever.cpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2025 Cotsbotics
+// Author: Casey Gregoire <caseyg@lalosoft.com>
+
+#include "sbus/sbus_receiver.hpp"
+
+namespace cotsbotics
+{
+
+void SbusReceiver::Begin()
+        {
+            /* Begin the SBUS communication */
+            sbus_rx.Begin();
+        }
+
+        void SbusReceiver::tick()
+        {
+            if (sbus_rx.Read()) {
+                /* Grab the received data */
+                bfs::SbusData new_data;
+                new_data = sbus_rx.data();
+                bool data_changed = false;
+                if (new_data.lost_frame != data.lost_frame) data_changed = true;
+                data.lost_frame = new_data.lost_frame;
+                if (new_data.failsafe != data.failsafe) data_changed = true;
+                data.failsafe = new_data.failsafe;
+                for (int8_t i = 0; i < data.NUM_CH; i++) {
+                    
+                    if (max(new_data.ch[i], data.ch[i]) - min(new_data.ch[i], data.ch[i]) > 2)
+                    {
+                        data_changed = true;
+                        data.ch[i] = new_data.ch[i];
+                    }
+                }
+                
+
+                if (data_changed)
+                {
+                    /* Display the received data */
+                    Serial.println("->");
+                    for (int8_t i = 0; i < data.NUM_CH; i++) {
+                        Serial.print(data.ch[i]);
+                        Serial.print("\t");
+                    }
+                    /* Display lost frames and failsafe data */                   
+                    Serial.print(data.lost_frame?"LF":"--");
+                    Serial.print("\t");
+                    Serial.println(data.failsafe?"FS":"--");
+                }
+                m_no_data_received=false;
+            }
+            else
+            {
+                m_no_data_received=true;
+            }
+        };
+
+}; // cotsbotics
